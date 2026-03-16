4fb7c9334f7bb2c218773dc4307e758db87ef8d5
Aayush Gupta <aayushgupta0403@gmail.com>
2026-03-16 17:10:16 +0530

    updated typing text

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<div>Hi, This is AAYUSH GUPTA</div> <span>I am B.Tech undergrad from Veer Surendra Sai University Of Technology in the branch of Information Technology ,currently i am in my Final year.<br><br> I am a Full-Stack Developer and also a passionate coder. i have a good knowledge of programming as well as web development,i also have done few projects as an intern and now looking for some further oppertunities. <br><br> My skills and projects  are mentioned below please have a look so that for work related quieries you can hire me!!</span> "
+        "<div>Hi, This is AAYUSH GUPTA</div> <span>A tech-driven Software Engineer currently working at iServeU, where I specialize in DevOps technologies, service monitoring, infrastructure optimization, and API gateway operations across hybrid environments (GCP, AWS & On-Prem).With hands-on experience in GCP, AWS, Kubernetes, Docker, Jenkins, Istio, Nagios, Prometheus, and Grafana, I help to ensure systems run smoothly, securely, and at peak efficiency. !!</span> "
       ],
       typeSpeed: 7,
       contentType: 'html'
