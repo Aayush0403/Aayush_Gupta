f3e4219c1fa0cc2f74ecc9d4f0e40a5c28eb469c
Aayush Gupta <aayushgupta0403@gmail.com>
2026-03-16 17:39:55 +0530

    updated the br tag

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
     $('.about-info-inner').typed({
       strings: [
-        "<div>Hi, This is AAYUSH GUPTA</div> <span>A tech-driven Software Engineer currently working at iServeU,<br/> where I specialize in DevOps technologies, service monitoring, infrastructure optimization, and API gateway operations across hybrid environments (GCP, AWS and On-Prem). <br/> With hands-on experience in GCP, AWS, Kubernetes, Docker, Jenkins, Istio, Nagios, Prometheus, and Grafana, I help to ensure systems run smoothly, securely, and at peak efficiency. !!</span> "
+        "<div>Hi, This is AAYUSH GUPTA</div> <span>A tech-driven Software Engineer currently working at iServeU,<br><br> I specialize in DevOps technologies, service monitoring, infrastructure optimization, and API gateway operations across hybrid environments (GCP, AWS and On-Prem). <br><br> With hands-on experience in GCP, AWS, Kubernetes, Docker, Jenkins, Istio, Nagios, Prometheus, and Grafana, I help to ensure systems run smoothly, securely, and at peak efficiency. !!</span> "
       ],
       typeSpeed: 7,
       contentType: 'html'
