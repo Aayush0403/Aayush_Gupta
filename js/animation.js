9f6b54fb4a968c713945219dd901041cd7f11b4f
Aayush Gupta <aayushgupta0403@gmail.com>
2026-03-16 17:08:32 +0530

    updated text array

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Aayush Gupta","Web developer","Freelancer","Problem solver"];
+const textArray = ["Aayush Gupta","DevOps Engineer","Site Reliability Engineer","Cloud Infrastructure Engineer ", "Automation Engineer"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
